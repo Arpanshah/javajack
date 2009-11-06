655243472c46a6a7af88ab26bec27875b7d22eab
dan.arnold <dan.arnold@e6a526a8-a4a2-11de-ac05-951101a8bf3e>
2009-11-06 23:04:30 +0000

    Added methods to this interface.

--- shared/src/PlayerListenerInterface.java
+++ shared/src/PlayerListenerInterface.java
@@ -1,11 +1,27 @@
+import java.util.List;
+
 /**
  * $Id$
  */
 
 /**
- * @author dan
+ * @author dan.arnold
  *
  */
 public interface PlayerListenerInterface {
-
+	void setCell( int gameId, int playerId, String value );
+	
+	void getCell( int gameId, int playerId );
+	
+	void setGames( List<Integer> gameList );
+	
+	void getGames();
+	
+	void getPlayers( int gameId );
+	
+	void joinGame( int gameId );
+	
+	void createGame( int seed );
+	
+	void getLastCardIndex( int gameId );
 }
