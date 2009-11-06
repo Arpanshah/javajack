ef048011eb0f72940457cd1bbf6e03354d854881
dan.arnold <dan.arnold@e6a526a8-a4a2-11de-ac05-951101a8bf3e>
2009-11-06 22:25:04 +0000

    Updated comments.

--- client/src/Javajack.java
+++ client/src/Javajack.java
@@ -13,7 +13,14 @@ public class Javajack {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Start GameBrowserGUI
+	}
+	
+	public Javajack( int seed, Dealer ) {
+		// Creates Deck from seed
+		// Gives Deck to Dealer
+		// Creates Dealer( 
 	}
 
 }
