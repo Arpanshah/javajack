5f8c07560b06386e9acd80823cbcae26846a6d5b
dan.arnold <dan.arnold@e6a526a8-a4a2-11de-ac05-951101a8bf3e>
2009-11-06 23:05:44 +0000

    Added method stubs.

--- client/src/ServerProxy.java
+++ client/src/ServerProxy.java
@@ -1,11 +1,74 @@
+import java.net.Socket;
+import java.util.List;
+
 /**
  * $Id$
  */
 
 /**
- * @author dan
+ * @author dan.arnold
  *
  */
 public class ServerProxy implements PlayerListenerInterface {
+
+	private static ServerProxy instance;
+	
+    public static ServerProxy getInstance() {
+        if ( instance == null )
+            instance = new ServerProxy();
+        return instance; 
+    } 
+	
+    private ServerProxy() {
+        ; // starts with the current time
+    }
+
+	@Override
+	public void createGame(int seed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getCell(int gameId, int playerId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getGames() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getLastCardIndex(int gameId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getPlayers(int gameId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void joinGame(int gameId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCell(int gameId, int playerId, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setGames(List<Integer> gameList) {
+		// TODO Auto-generated method stub
+		
+	}  
 
 }
