ae3a097ec44dcded5f86432e79a292287de20606
dan.arnold <dan.arnold@e6a526a8-a4a2-11de-ac05-951101a8bf3e>
2009-11-06 20:33:20 +0000

    Created stubs for everything.

---
+++ trunk/server/src/Server.java
@@ -0,0 +1,19 @@
+/**
+ * $Id$
+ */
+
+/**
+ * @author dan.arnold
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
