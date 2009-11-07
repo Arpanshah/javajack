4f07e1959657e2e6fb416e8c5b034150aa754fb8
dan.arnold <dan.arnold@e6a526a8-a4a2-11de-ac05-951101a8bf3e>
2009-11-07 04:30:19 +0000

    local player fixed!

--- trunk/client/src/LocalPlayer.java
+++ trunk/client/src/LocalPlayer.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * $Id$
  */
@@ -31,8 +33,49 @@ public class LocalPlayer implements PlayerInterface {
 		return id;
 	}
 
-	@Override
 	public void setBet(int bet, int id, int gameId) {
 		// I'm local, ignore
+	}
+
+	@Override
+	public int getLastCardIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Integer> getPlayers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setResult(int result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tellBet(int playerId, int amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tellCard(int playerId, int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tellLastCardIndex(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tellPlayers(List<Integer> playerIds) {
+		// TODO Auto-generated method stub
+		
 	}
 }
