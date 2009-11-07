7aee795deef1219eb6082b71769bbdad5986fa49
seongyupyoo <seongyupyoo@e6a526a8-a4a2-11de-ac05-951101a8bf3e>
2009-11-07 03:28:50 +0000

    proxies done

--- trunk/server/src/ClientProxy.java
+++ trunk/server/src/ClientProxy.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.List;
@@ -15,6 +17,7 @@ public class ClientProxy implements ServerModelListenerInterface {
 
 	// Private members
 	private Socket socket;
+	private BufferedReader in;
 	private PrintStream out;
 	private PlayerListenerInterface listener;
 	
@@ -25,6 +28,7 @@ public class ClientProxy implements ServerModelListenerInterface {
 	public ClientProxy(Socket socket) {
 		this.socket = socket;
 		try {
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new PrintStream(socket.getOutputStream(), true);
 		} catch (IOException e) {
 			System.err.println(socket.getInetAddress().toString().substring(1) + ":" + socket.getPort() +  " <-- I/O error");
@@ -119,6 +123,33 @@ public class ClientProxy implements ServerModelListenerInterface {
 			throw new NullPointerException();
 		}
 		this.listener = listener;
+	}
+	
+	private class Reader extends Thread {
+		public void run() {
+			try {
+				while(true) {
+					String [] tokens = in.readLine().split(" ");
+					String command = tokens[0];
+					
+					if(command.equals("createGame")) {
+						listener.createGame(Integer.parseInt(tokens[1]));
+					} else if (command.equals("getCell")) {
+						listener.getCell(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+					} else if(command.equals("getGames")) {
+						listener.getGames();
+					} else if (command.equals("getLastCardIndex")) {
+						listener.getLastCardIndex(Integer.parseInt(tokens[1]));
+					} else if(command.equals("getPlayers")) {
+						listener.getPlayers(Integer.parseInt(tokens[1]));
+					} else if (command.equals("joinGame")) {
+						listener.joinGame(Integer.parseInt(tokens[1]));
+					} else if(command.equals("setCell")) {
+						listener.setCell(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), tokens[3]);
+					}
+				}
+			} catch (Exception e) {}
+		}
 	}
 }
 
